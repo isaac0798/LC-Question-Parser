3c37ec522777a0cdebe6a3343292a42885744cc6
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2022-03-19 11:12:25 +0000

    feature/count each topic type and put in hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -28,9 +29,16 @@ fn main() {
     let question_path = "src/questions.json";
     let questions = File::open(question_path).expect("Cant find path");
 
+    let mut topicCount: HashMap<&String, i128> = HashMap::new();
     let questions: Vec<Question> = serde_json::from_reader(questions).expect("Unable to parse");
     
     for question in questions.iter() {
-        println!("Title {}", question.title);
+        for topicTag in question.topicTags.iter() {
+            *topicCount.entry(&topicTag.name).or_insert(0) += 1;
+        }
+    }
+
+    for (key, value) in &topicCount {
+        println!("{} / {}", key, value);
     }
 }
